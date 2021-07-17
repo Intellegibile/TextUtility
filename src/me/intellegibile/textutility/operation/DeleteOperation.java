6130922ab234d196b7977cb74237c0d358147814
Intellegibile <78700221+Intellegibile@users.noreply.github.com>
2021-07-17 17:13:31 +0200

    Refactoring
    Risoluzione conflitti git

--- src/me/intellegibile/textutility/operation/DeleteOperation.java
+++ src/me/intellegibile/textutility/operation/DeleteOperation.java
@@ -1,39 +1,21 @@
 package me.intellegibile.textutility.operation;
 
+import me.intellegibile.textutility.file.InputFile;
+import me.intellegibile.textutility.file.OutputFile;
+
 import java.io.*;
 import java.util.ArrayList;
 
 public class DeleteOperation extends Operation{
-    private ArrayList<Character> first = new ArrayList<Character>();
+    private final String word;
 
-    public DeleteOperation(String first_, FileInputStream input_, FileOutputStream output_) {
-        super(input_, output_);
-        for (char c : first_.toCharArray()) {
-            first.add(c);
-        }
-    }
-
-    public void delete() {
-        while (this.isRunning()) {
-            this.word(this.start);
-            if (this.wordArray.equals(this.first)) {
-                for (int l = this.startWord; l < this.finishWord; l++) {
-                    this.charactersMedium.set(l, null);
-                }
-            }
-            this.updateStart();
-            this.clearWordArray();
-        }
+    public DeleteOperation(String word, InputFile inputFile, OutputFile outputFile) {
+       super(inputFile, outputFile);
+       this.word = word;
     }
 
     @Override
     public void operate() {
-       this.delete();
-       this.mediumToOutput();
-       StringBuilder stringBuilder = new StringBuilder();
-       for (int i = 0; i < this.charactersOutput.length; i++) {
-           stringBuilder.append((char)this.charactersOutput[i]);
-       }
-       this.outputFile();
+        
     }
 }
