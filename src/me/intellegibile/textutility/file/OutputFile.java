e3572ba94ff90d65691ac32c7d8253e7e7bb45e8
Intellegibile <78700221+Intellegibile@users.noreply.github.com>
2021-07-17 16:52:37 +0200

    Update OutputFile.java
    Aggiunto costruttore di OutputFile

--- src/me/intellegibile/textutility/file/OutputFile.java
+++ src/me/intellegibile/textutility/file/OutputFile.java
@@ -1,4 +1,11 @@
 package me.intellegibile.textutility.file;
 
+import java.io.FileOutputStream;
+
 public class OutputFile {
+    private final FileOutputStream fileOutputStream;
+
+    public OutputFile(FileOutputStream outputStream) {
+        this.fileOutputStream = outputStream;
+    }
 }
