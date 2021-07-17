71e74b94d09d9d73e2bbebc7bf76031ff77f03ce
Intellegibile <78700221+Intellegibile@users.noreply.github.com>
2021-07-17 16:55:13 +0200

    Risoluzioni conflitti git
    Risoluzione conflitti di git

--- src/me/intellegibile/textutility/operation/DeleteOperation.java
+++ src/me/intellegibile/textutility/operation/DeleteOperation.java
@@ -1,16 +1,25 @@
 package me.intellegibile.textutility.operation;
 
-import java.io.*;
+import me.intellegibile.textutility.file.InputFile;
+import me.intellegibile.textutility.file.OutputFile;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class DeleteOperation extends Operation{
     private ArrayList<Character> first = new ArrayList<Character>();
 
-    public DeleteOperation(String first_, FileInputStream input_, FileOutputStream output_) {
-        super(input_, output_);
+    public DeleteOperation(String first_, InputFile inputFile, OutputFile outputFile) {
+        super(inputFile, outputFile);
         for (char c : first_.toCharArray()) {
             first.add(c);
         }
+    }
+
+    public void deleteAllSpaces() {
+        this.getStringReader().spacePositions();
+        List<Integer> spacePosition = this.getStringReader().getSpacePositions();
+        System.out.println(spacePosition);
     }
 
     @Override
