264f766cdb3224ccfc55992f7cac300f1650f3a9
Intellegibile <78700221+Intellegibile@users.noreply.github.com>
2021-07-18 16:41:52 +0200

    Refactoring and update
    Refactoring dell'operazione Delete e update delle sue possibilità

--- src/me/intellegibile/textutility/operation/DeleteOperation.java
+++ src/me/intellegibile/textutility/operation/DeleteOperation.java
@@ -3,20 +3,30 @@ package me.intellegibile.textutility.operation;
 import me.intellegibile.textutility.file.InputFile;
 import me.intellegibile.textutility.file.OutputFile;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DeleteOperation extends Operation{
     private final String word;
+    private final char[] chars;
+    private List<Character> characterList = new ArrayList<Character>();
 
     public DeleteOperation(String word, InputFile inputFile, OutputFile outputFile) {
        super(inputFile, outputFile);
        this.word = word;
+       this.chars = inputFile.toString().toCharArray();
+       for (int i = 0; i < this.chars.length; i++) {
+           this.characterList.add(this.chars[i]);
+       }
+    }
+
+    public void deleteAllSpaces() {
+        this.characterList = this.characterList.stream().filter(character -> !Character.isWhitespace(character.charValue())).toList();
     }
 
     @Override
     public void operate() {
-
+        this.deleteAllSpaces();
+        this.setOutputString(this.characterList);
     }
 }
