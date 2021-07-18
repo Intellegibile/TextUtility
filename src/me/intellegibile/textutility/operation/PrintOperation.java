ee77d473744bb7286d04304e35bce762bed44b05
Intellegibile <78700221+Intellegibile@users.noreply.github.com>
2021-07-18 16:42:21 +0200

    Refactoring

--- src/me/intellegibile/textutility/operation/PrintOperation.java
+++ src/me/intellegibile/textutility/operation/PrintOperation.java
@@ -3,10 +3,6 @@ package me.intellegibile.textutility.operation;
 import me.intellegibile.textutility.file.InputFile;
 import me.intellegibile.textutility.file.OutputFile;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.nio.charset.StandardCharsets;
-
 public class PrintOperation extends Operation{
 
     public PrintOperation(InputFile inputFile, OutputFile outputFile) {
