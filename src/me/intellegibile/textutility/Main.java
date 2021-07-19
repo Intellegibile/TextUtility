b669e4ca6b8974cc755ed8b0a29b3bf667b8dd7d
Intellegibile <78700221+Intellegibile@users.noreply.github.com>
2021-07-19 14:53:37 +0200

    Refactoring using CommandPattern

--- src/me/intellegibile/textutility/Main.java
+++ src/me/intellegibile/textutility/Main.java
@@ -1,81 +1,12 @@
 package me.intellegibile.textutility;
 
-import me.intellegibile.textutility.file.InputFile;
-import me.intellegibile.textutility.file.OutputFile;
-import me.intellegibile.textutility.operation.Operation;
-import me.intellegibile.textutility.operation.PrintOperation;
-import me.intellegibile.textutility.operation.DeleteOperation;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.util.Scanner;
+import me.intellegibile.textutility.Pattern.CommandPattern;
 
 public class Main {
 
-    public static boolean isReady() {
-        System.out.println("Your file will be overwritten, write Y to continue or N to stop");
-        boolean ready = false;
-        Scanner scanner = new Scanner(System.in);
-        String yn = scanner.next();
-        String yes = "Y";
-        String no = "N";
-        if (yn.equals(yes)) {
-            ready = true;
-        } else if (yn.equals(no)) {
-            ready = false;
-        } else {
-            System.out.println("Incorrect argument");
-            isReady();
-        }
-        return ready;
-    }
-
     public static void main(String[] args) {
-        if (args.length < 1) {
-            throw new IllegalArgumentException("Usage: <operation> <arguments> <input> (Optional)<output>");
-        }
-
-        switch (args[0]) {
-            case "print":
-                if (args.length == 2) {
-                        if (args[1].endsWith(".txt")) {
-                            PrintOperation printOperation = new PrintOperation(new InputFile(Path.of(args[1])), null);
-                            printOperation.operate();
-                        }
-                } else if (args.length > 2) {
-                    if (args[2].endsWith(".txt")) {
-                        throw new IllegalArgumentException("PrintOperation does not require an output file");
-                    } else {
-                        throw new IllegalArgumentException("You specified an existent argument");
-                    }
-                } else if (args.length < 2) {
-                    throw new IllegalArgumentException("You need to define the path of a file to print");
-                }
-            case "delete":
-                if (args.length == 4) {
-                        if (args[2].endsWith(".txt") && args[3].endsWith(".txt")) {
-                            DeleteOperation deleteOperation = new DeleteOperation(args[1], new InputFile(Path.of(args[2])), new OutputFile(Path.of(args[3])));
-                            deleteOperation.operate();
-                        } else {
-                            throw  new IllegalArgumentException("You need to define the path of the file input and output");
-                        }
-                } else if (args.length == 3) {
-                    if (isReady()) {
-                            if (args[2].endsWith(".txt")) {
-                                DeleteOperation deleteOperation = new DeleteOperation(args[1], new InputFile(Path.of(args[2])), new OutputFile(Path.of(args[2])));
-                                deleteOperation.operate();
-                            } else {
-                                throw new IllegalArgumentException("You need to define the path of the file input");
-                            }
-                    }
-                } else if (args.length > 4) {
-                    throw new IllegalArgumentException("You specified an argument not needed");
-                } else {
-                    throw new IllegalArgumentException("You specified less arguments");
-                }
-        }
+        CommandPattern commandPattern = new CommandPattern(args);
+        commandPattern.parse();
     }
+
 }
